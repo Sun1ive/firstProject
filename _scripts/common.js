8e867a9aa4f08cab06031411e2da07fca06e6abc
Sun1ive <sunliveua@gmail.com>
2017-07-17 23:00:03 +0300

    Add files via upload

---
+++ _scripts/common.js
@@ -0,0 +1,42 @@
+$(window).on("load", function () {
+
+    $(".tab-h").on("click", function (event) {
+       var that = $(this);
+
+       if (that.hasClass("tab-h")) {
+        var dataTab = $(this).attr("data-tab");
+
+        var tabH = $(".tab-h");
+        for (var i = 0; i < tabH.length;i++) {
+            $(tabH[i]).removeClass("active");
+        }
+
+        that.addClass("active");
+        var tabBody = $('.tab-b');
+        for (var i = 0; i < tabBody.length; i++) {
+            if (dataTab == i) {
+                $(tabBody[i]).css("display", "block");
+            } else {
+            $(tabBody[i]).css("display", "none");
+            }
+       }
+        }
+    });
+
+
+});
+
+$(window).on("load", function(){
+
+    var winHeight = $(document).height();
+    var step = 2;
+    var timeToScroll = winHeight/step;
+
+    $('.scrollTop').on('click', function(){
+
+        $('html, body').animate({
+            scrollTop: 0
+        }, timeToScroll);
+    });
+
+});
